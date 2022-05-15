9d29405188f2f5c53d585370c16ec2a2063ab1d9
NewChakrit <imnew108@gmail.com>
2022-05-15 07:52:02 -0700

    'getallhotels-limit-propertylist-featuredprops'

--- controllers/hotelController.js
+++ controllers/hotelController.js
@@ -41,8 +41,15 @@ export const getHotel = async (req, res, next) => {
   }
 };
 export const getAllHotels = async (req, res, next) => {
+  const { min, max, ...others } = req.query;
   try {
-    const hotels = await Hotel.find();
+    const hotels = await Hotel.find({
+      others,
+      cheapestPrice: {
+        $gt: min | 1,
+        $lt: max | 999,
+      },
+    }).limit(req.query.limit);
     res.status(200).json(hotels);
   } catch (err) {
     next(err);
